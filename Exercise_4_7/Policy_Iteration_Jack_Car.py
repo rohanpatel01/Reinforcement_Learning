040e338b768fce396d6f37aa4ce9c80dcc048096
rohanpatel01 <rohanppatel01@gmail.com>
2025-05-27 11:58:02 -0500

    Policy Iteration in progress

--- Exercise_4_7/Policy_Iteration_Jack_Car.py
+++ Exercise_4_7/Policy_Iteration_Jack_Car.py
@@ -1,66 +1,17 @@
 
 
-'''
-Policy iteration to re-solve Jack's car rental problem
-
-First I will replicate results for original problem
-
-Second I will add changes as dictated by exercise 4.7
-
-'''
-
 import numpy as np
 import itertools
 
-'''
-Notes for original Jack's Car Rental Problem
-
-Jack will rent out all cars available at a location and get +10 for each one
-Jack has the option to take action to move cars but at reward of -2*num cars moved
-
-+$10 for renting out a car
--$2/car moved overnight
-
-Number of cars requested at each location: Poisson RV with lambda = 3 (first) 4 (second) location
-Returned:  Poisson RV with lambda = 3 (first) 2 (second) location
-
-Max 20 cars at each location
-gamma = 0.9
-continuous finine MDP:
-- time steps = days
-- state = [num cars location 1 end of day, num cars location 2 end of day]
-- actions = [net num cars moved from/to location 1, net num cars moved from/to location 2]
-
-from: - #
-to:   + #
-'''
-
 MAX_CARS_IN_LOCATION = 20
 NUM_STATES = (MAX_CARS_IN_LOCATION + 1) * 2       # plus 1 to include zero
-NUM_ACTIONS = 11 # (-5 to -1) and (0 to 5)
+NUM_ACTIONS = 11 # (0 to 5) and (-1 to -5)
 
-REQUEST_LAMBDA_A = 3
-REQUEST_LAMBDA_B = 4
+RETURN_LAMBDA_A = 3     # max = 8
+REQUEST_LAMBDA_A = 3    # max = 8
 
-RETURN_LAMBDA_A = 3
-RETURN_LAMBDA_B = 2
-
-
-
-"""
-# max 5 cars can be moved from one location to other
-
-# This action can be taken from any state and considers moving cars between both locations
-A0 = +0
-A1 = +1
-...
-A5 = +5
-
-A6 = -1 
-...
-A10 = -5
-"""
-
+RETURN_LAMBDA_B = 2     # max = 6
+REQUEST_LAMBDA_B = 4    # max = 10
 
 def compute_p():
     pass
@@ -76,13 +27,13 @@ def next_state_probability(current_state, next_state, policy):
     # go over all reasonable values reqA, reqB, retA, retB can take and sum their prob of getting to next state
     # 99.9% of the probability mass lies within 0 to lambda + 4*sqrt(lambda)
 
-    reqA_range = range(0, REQUEST_LAMBDA_A+(4*np.sqrt(REQUEST_LAMBDA_A)))
-    reqB_range = range(0, REQUEST_LAMBDA_B+(4*np.sqrt(REQUEST_LAMBDA_B)))
+    reqA_range = range(0, int(REQUEST_LAMBDA_A+(4*np.sqrt(REQUEST_LAMBDA_A))))
+    reqB_range = range(0, int(REQUEST_LAMBDA_B+(4*np.sqrt(REQUEST_LAMBDA_B))))
 
-    retA_range = range(0, RETURN_LAMBDA_A+(4*np.sqrt(RETURN_LAMBDA_A)))
-    retB_range = range(0, RETURN_LAMBDA_B+(4*np.sqrt(RETURN_LAMBDA_B)))
+    retA_range = range(0, int(RETURN_LAMBDA_A+(4*np.sqrt(RETURN_LAMBDA_A))))
+    retB_range = range(0, int(RETURN_LAMBDA_B+(4*np.sqrt(RETURN_LAMBDA_B))))
 
-    for retA, retB, reqA, reqB in itertools.product(reqA_range, reqB_range, retA_range, retB_range):
+    # for retA, retB, reqA, reqB in itertools.product(reqA_range, reqB_range, retA_range, retB_range):
 
 
 
@@ -92,42 +43,33 @@ def next_state_probability(current_state, next_state, policy):
 def policy_evaluation(policy, values, theta, gamma ):
 
     delta = float('inf')
+    cars_A_range = range(MAX_CARS_IN_LOCATION + 1)
+    cars_B_range = range(MAX_CARS_IN_LOCATION + 1)
+
+    return_A_range = range(0, int(RETURN_LAMBDA_A + (3 * np.sqrt(RETURN_LAMBDA_A))))
+    request_A_range = range(0, int(REQUEST_LAMBDA_A + (3 * np.sqrt(REQUEST_LAMBDA_A))))
+
+    return_B_range = range(0, int(RETURN_LAMBDA_B + (3 * np.sqrt(RETURN_LAMBDA_B))))
+    request_B_range = range(0, int(REQUEST_LAMBDA_B + (3 * np.sqrt(REQUEST_LAMBDA_B))))
 
     while not (delta < theta):
+
         delta = 0
 
-        cars_A_range = range(MAX_CARS_IN_LOCATION + 1)
-        cars_B_range = range(MAX_CARS_IN_LOCATION + 1)
-
-        # loop through all states, to compute value of a state, to get p: only consider the next states that are likely
-
-        # Loop over all states s
         for cars_A, cars_B in itertools.product(cars_A_range, cars_B_range):
-
-            # Loop over all next states s'
             for cars_A_next, cars_B_next in itertools.product(cars_A_range, cars_B_range):
 
-                # check if this state is possible
-                # ^^^ if looping over all possible values of RVs do not result in next state then it is not possible and continue, else continue update computation
+                v = values[cars_A][cars_B]
+                values[cars_A][cars_B] = 0      # set to zero for new computation
+                action_A, action_B = actions[policy[cars_A][cars_B]]
+                expected_p = 0.0     # compute expected probability of s'
+                expected_r = 0.0     # compute expected reward from s->s'
 
-                p = next_state_probability((cars_A, cars_B), (cars_A_next, cars_B_next), policy)
-                if p == 0: continue
-
-                # state is possible so continue computation and return
-
+                for returnA, requestA, returnB, requestB in itertools.product(return_A_range, request_A_range, return_B_range, request_B_range):
+                    expected_p += np.random.poisson(returnA)*np.random.poisson(requestA)*np.random.poisson(returnB)*np.random.poisson(requestB)
+                    expected_r += (-2*np.abs())
 
 
-                # TODO: Loop over all possible next states
-                '''
-                Loop over all possible values I, G, J, H can take
-                If they
-                '''
-
-                # TODO: Figure out again why we need to loop over all the actions
-                # TODO: I think I just confused but I need a way to get Pi(s)
-                for action_A, action_B in actions:
-                    v = values[(cars_first_location, cars_second_location)]
-                    p = compute_p()
 
 
 
@@ -138,39 +80,17 @@ def policy_iteration(policy, values):
 if __name__ == '__main__':
 
     # deterministic policy so policy just gives us the action to take in the given state
-    policy = np.zeros(NUM_STATES)
-    values = np.zeros(NUM_STATES)
-    actions = np.array()
-
-    actions = []
+    policy = np.zeros(shape=(NUM_STATES, NUM_STATES), dtype=int) # Policy maps states [(Cars in A, Cars in B)] => Action index
+    values = np.zeros(shape=(NUM_STATES, NUM_STATES), dtype=float)
+    actions = []                    # Actions contains all pairs of valid actions, accessed by index given by policy
 
     for action_A, action_B in itertools.product(range(-5, 6), repeat=2):
         if action_A + action_B == 0:
             actions.append((action_A, action_B))
 
-    actions = np.array(actions)
+    actions.sort(key=lambda x: (x[0] < 0, abs(x[0])))
+    actions = np.array(actions, dtype=int)
 
     new_values = policy_evaluation(policy, values, 2, 0.9)
-
-
-    # reward for a state comes from: total_num_cars*10 + (-2*cars_moved)
-    # have table for all possible states and populate them?
-    # env is not deterministic because values are poisson rv
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
